068e3be266a64261e460d2cd19edf0254f56c54d
Charles Ma <chuckjma@gmail.com>
2011-05-05 20:25:22 -0700

    Edited xmonad.hs via GitHub

--- xmonad.hs
+++ xmonad.hs
@@ -2,11 +2,12 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
 
 main = do
-h <- spawnPipe "xmobar"
-xmonad $ defaultConfig {
+h <- spawnPipe "xmobar"                                                                                                                                                                                                                      
+xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
    logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn h }
    , manageHook = manageDocks <+> manageHook defaultConfig
    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-}   
+}
